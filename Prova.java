262dd64a2a4eef3cee8438380d0b6dca45fe084d
Kateryna Romanyuk <kateryna.romanyuk20@gmail.com>
2022-06-08 17:28:10 +0200

    Add method, remove method, change method

--- Prova.java
+++ Prova.java
@@ -5,8 +5,18 @@ public class Prova{
     public Prova(){}
 
     public void method_to_change(){
-        System.out.println("Ciao!");
+        System.out.println("Un'altra stringa");
+        System.out.println("Un'altra ancora");
+
+        int y = 3;
+        int x = 4;
+
+        int sum = x + y;
+
+        System.out.println("La somma di " + x + " e " + y + " e' " + sum);
     }
 
-    public void method_to_delete(){}
+    public void method_added(String text){
+        System.out.println(text);
+    }
 }
